ef97a29ee4e7bfb3114d50b932afc03fa26d50c8
kozikow <kozikow@kozikow-ThinkPad-W520.(none)>
2012-09-17 04:05:07 +0200

    costam dziala jeszcze

--- js/app.js
+++ js/app.js
@@ -7,7 +7,8 @@ App.ApplicationView = Ember.View.extend({
   }
 });
 App.ApplicationController = Ember.Controller.extend({
-  algorithm : "reverse",
+  algorithm : undefined,
+  in_algorithm : undefined,
 
   force : undefined,
   nodes : [],
@@ -33,7 +34,7 @@ App.ApplicationController = Ember.Controller.extend({
     this.set("algorithm", "slyce");
   },
 
-  add_node : function(event) {
+  addNode : function(event) {
     var nodes = this.get("nodes"),
         links = this.get("links"),
         point = d3.mouse(event),
@@ -53,6 +54,50 @@ App.ApplicationController = Ember.Controller.extend({
     this.set("links", links);
   },
 
+  forceTick : function() {
+    var vis = this.get("vis");
+    vis.selectAll("line.link")
+        .attr("x1", function(d) { return d.source.x; })
+        .attr("y1", function(d) { return d.source.y; })
+        .attr("x2", function(d) { return d.target.x; })
+        .attr("y2", function(d) { return d.target.y; })
+        .style("stroke-width", function(d, i) {
+          if( ($.inArray(d.source.index, removed_nodes) != -1) ) {
+            return 1;
+          }
+          if( ($.inArray(d.target.index, removed_nodes) != -1) ) {
+            return 1;
+          }
+          else {
+            return 5;
+          }
+        });
+
+    vis.selectAll("text.node")
+      .attr("x", function(d) { return d.x - 5; })
+      .attr("y", function(d) { return d.y + 5; })
+      .style("fill-opacity", function(d,i) {
+        if($.inArray(i, removed_nodes) != -1) {
+          return 0.2;
+        }
+        else {
+          return 1;
+        }
+       });
+
+    vis.selectAll("circle.node")
+      .attr("cx", function(d) { return d.x; })
+      .attr("cy", function(d) { return d.y; })
+      .style("fill-opacity", function(d,i) {
+        if($.inArray(i, removed_nodes) != -1) {
+          return 0.1;
+        }
+        else {
+          return 0.4;
+        }
+       })
+  },
+
   restart : function() {
     var force = this.get("force"),
         vis = this.get("vis"),
@@ -63,59 +108,43 @@ App.ApplicationController = Ember.Controller.extend({
     vis.selectAll("line.link").data(links)
     .enter().insert("line", "circle.node")
       .attr("class", "link")
-      .attr("x1", function(d) { return d.source.x; })
-      .attr("y1", function(d) { return d.source.y; })
-      .attr("x2", function(d) { return d.target.x; })
-      .attr("y2", function(d) { return d.target.y; })
       .style("fill", "black")
-      .style("stroke-width", function(d, i) {
-        if( ($.inArray(d.source.index, removed_nodes) != -1) ) {
-          return 1;
-        }
-        if( ($.inArray(d.target.index, removed_nodes) != -1) ) {
-          return 1;
-        }
-        else {
-          return 5;
-        }
-      })
 
     vis.selectAll("circle.node")
       .data(nodes)
-    .enter().insert("circle", "circle.cursor")
+      .enter().insert("circle", "circle.cursor")
       .attr("class", "node")
-      .attr("cx", function(d) { return d.x; })
-      .attr("cy", function(d) { return d.y; })
       .attr("r", 15)
-      .style("fill-opacity", function(d,i) {
-        if($.inArray(i, removed_nodes) != -1) {
-          return 0.1;
-        }
-        else {
-          return 0.4;
-        }
-       })
+      
       .call(force.drag);
 
     vis.selectAll("text.node")
       .data(nodes)
     .enter().append("svg:text")
       .attr("class", "node")
-      .attr("x", function(d) { return d.x - 5; })
-      .attr("y", function(d) { return d.y + 5; })
       .text(function(d, i) { return i; })
       .style("fill","black")
       .style("font-size", function(d) { return "20px"; })
-      .style("fill-opacity", function(d,i) {
-        if($.inArray(i, removed_nodes) != -1) {
-          return 0.2;
-        }
-        else {
-          return 1;
-        }
-       })
       .call(force.drag);
   }.observes("algorithm", "num_nodes"),
+
+  setCursor : function() {
+    var vis = this.get("vis");
+    var in_algorithm = this.get("in_algorithm");
+    if(!in_algorithm) {
+      var cursor = vis.append("circle")
+      .attr("r", 100)
+      .attr("transform", "translate(-100,-100)")
+      .attr("class", "cursor");
+
+      vis.on("mousemove", function() {
+        cursor.attr("transform", "translate(" + d3.mouse(this) + ")");
+      });
+    }
+    else {
+      vis.selectAll("circle").remove();
+    }
+  }.observes("in_algorithm"),
 
   drawGraph : function() {
     var self = this,
@@ -142,50 +171,20 @@ App.ApplicationController = Ember.Controller.extend({
       .links(links)
       .size([width, height]);
 
-    var cursor = vis.append("circle")
-      .attr("r", 100)
-      .attr("transform", "translate(-100,-100)")
-      .attr("class", "cursor");
-
     force.on("tick", function() {
-      vis.selectAll("line.link")
-        .attr("x1", function(d) { return d.source.x; })
-        .attr("y1", function(d) { return d.source.y; })
-        .attr("x2", function(d) { return d.target.x; })
-        .attr("y2", function(d) { return d.target.y; })
-        .style("stroke-width", function(d, i) {
-          if( ($.inArray(d.source.index, removed_nodes) != -1) ) {
-            return 1;
-          }
-          if( ($.inArray(d.target.index, removed_nodes) != -1) ) {
-            return 1;
-          }
-          else {
-            return 5;
-          }
-        });
-
-      vis.selectAll("text.node")
-        .attr("x", function(d) { return d.x - 5; })
-        .attr("y", function(d) { return d.y + 5; });
-
-      vis.selectAll("circle.node")
-        .attr("cx", function(d) { return d.x; })
-        .attr("cy", function(d) { return d.y; });
-    });
-
-    vis.on("mousemove", function() {
-      cursor.attr("transform", "translate(" + d3.mouse(this) + ")");
+      self.forceTick();
     });
 
     vis.on("mousedown", function() {
-      self.add_node(this);
+      self.addNode(this);
     });
 
     this.set("nodes", nodes),
     this.set("links", links),
     this.set("vis", vis),
     this.set("force", force);
+    this.set("in_algorithm", false)
+    this.set("algorithm", "reverse")
   }
 });
 
